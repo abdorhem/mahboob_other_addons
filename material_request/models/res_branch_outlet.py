4cd700ca13949ea45e41d0580a98a7f464fa31a6
abdorhem <33388511+abdorhem@users.noreply.github.com>
2022-03-26 23:47:22 +0300

    Delete material_request directory

--- material_request/models/res_branch_outlet.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResBranchOutlet(models.Model):
-    _inherit = "res.branch.outlet"
-    warehouse_manager_id = fields.Many2one("hr.employee", "Warehouse Manager")
